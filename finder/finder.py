44bacf3ab17ef8d00e327c64aacdfd2cfac79556
Mateusz Flieger <mateuszflieger@gmail.com>
2012-11-20 20:19:24 +0100

    Imie i miasto

--- finder/finder.py
+++ finder/finder.py
@@ -1,19 +1,34 @@
 class finder:
+    """Potwierdza presupozycje pytania"""
     def __init__(self):
-        self.dane=[]
-        self.ignore={'Kto','kto'}
-        self.place = ''
+        self.name = ''
+        self.place = []
 
+    # Jesli UpperCase przed w to jest to imie(nazwisko), inaczej po w ostatni upper to imie(nazwisko)
     def find(self,zdanie):
-        import re
-        for s in zdanie.split():
-            if s[0].isupper() and s not in self.ignore: 
-                self.dane.append(s)
-                if re.match(".*w %s" % s, zdanie): self.place = s
-#        return self.dane
+        index = zdanie.find("w")
+        city = zdanie[index+1:] #zdanie po 'w' 
+        self.place = city.split() #mozliwe miasta
+        for s in zdanie[:index].split(): #czy jest przed w cos z duzej?
+            if s[0].isupper() and s != "Kto": 
+                self.name = s
+        if not self.name: #jesli nie ma
+            self.name = self.place[-1]
+            self.place = self.place[:-1]
 
     def checker(self):
         for line in open("../bazy/finbaza.fred"):
-            for word in self.dane:
-                if line.startswith(word):
-                    print(line)
+            if line.startswith(" ".join(self.place)):
+                print(line)
+        print(self.name, " ".join(self.place))
+
+
+x = finder()
+x.find("Kto zabil Kennediego w Dallas")
+x.checker()
+y = finder()
+y.find("Kto zabil w Dallas Kennediego")
+y.checker()
+z = finder()
+z.find("Kto zabil Fliegera w Santa Clara")
+z.checker()
