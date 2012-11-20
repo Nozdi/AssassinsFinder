18e0b13976e320bae8179658dc97844f1ac00a81
Mateusz Flieger <mateuszflieger@gmail.com>
2012-11-20 18:43:30 +0100

    OMG LOL

--- finder/finder.py
+++ finder/finder.py
@@ -1,24 +1,19 @@
 class finder:
     def __init__(self):
-<<<<<<< HEAD
-        lf.ignore= {'kto', 'Kto'}
+        self.dane=[]
+        self.ignore={'Kto','kto'}
 
     def find(self,zdanie):
         self.dane=[]
         for s in zdanie.split():
             if s[0].isupper() and s not in self.ignore: self.dane.append(s)
         return self.dane
-
-=======
-        self.dane=[]
-        self.ignore={'Kto','kto'}
 
     def find(self,zdanie):
         for s in zdanie.split():
             if s[0].isupper() and s not in self.ignore: self.dane.append(s)
         return self.dane
     
->>>>>>> ca9b608c76604aa9503b99863ede033db82cd356
     def checker(self, duze):
         import os	
         print(duze)
@@ -27,9 +22,5 @@ class finder:
             f2 = open("tmp.txt")
             tmp=f2.readline()
             if int(tmp)>0:
-<<<<<<< HEAD
-                print(i,"JEST!")		
-=======
                 print(i,"JEST!")
 		
->>>>>>> ca9b608c76604aa9503b99863ede033db82cd356
