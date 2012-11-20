33f3e9991c732ff8d814830da2e8f05010adb7fc
Mateusz Flieger <mateuszflieger@gmail.com>
2012-11-20 18:33:08 +0100

    Fred's strange spaces

--- finder/finder.py
+++ finder/finder.py
@@ -1,21 +1,19 @@
 class finder:
-	def __init__(self):
-		lf.ignore= {'kto', 'Kto'}
+    def __init__(self):
+        lf.ignore= {'kto', 'Kto'}
 
-	def find(self,zdanie):
-        	self.dane=[]
-		for s in zdanie.split():
-			if s[0].isupper() and s not in self.ignore: self.dane.append(s)
-		return self.dane
+    def find(self,zdanie):
+        self.dane=[]
+        for s in zdanie.split():
+            if s[0].isupper() and s not in self.ignore: self.dane.append(s)
+        return self.dane
 
-    
-	def checker(self, duze):
-		import os	
-		print(duze)
-		for i in duze:
-			os.system("grep "+i+" ../bazy/finbaza.fred | wc -l > tmp.txt")
-			f2 = open("tmp.txt")
-			tmp=f2.readline()
-			if int(tmp)>0:
-				print(i,"JEST!")    
-		
+    def checker(self, duze):
+        import os	
+        print(duze)
+        for i in duze:
+            os.system("grep "+i+" ../bazy/finbaza.fred | wc -l > tmp.txt")
+            f2 = open("tmp.txt")
+            tmp=f2.readline()
+            if int(tmp)>0:
+                print(i,"JEST!")		
