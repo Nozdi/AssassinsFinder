a31e24bf9d95f9b70eda803685a78f59e20c55b1
Mateusz Flieger <mateuszflieger@gmail.com>
2012-11-21 16:34:08 +0100

    teksty + zmiany w finder

--- finder/finder.py
+++ finder/finder.py
@@ -28,7 +28,8 @@ def odmiany_synonimow(synlist):
     return ', '.join(ret).replace("\n", "").split(', ')
 
 text = podaj_zdania(open("Kennedy.txt").read())
-
+#text = podaj_zdania(open("narutowicz.txt").read())
+#text = podaj_zdania(open("lennon.txt").read())
 def znajdz_czas():
     ret = []
     with open("./bazy/zabil.all") as o:
@@ -36,7 +37,7 @@ def znajdz_czas():
     for elem in text:
         for slowo in zabic:
             if slowo in elem:
-                ret.append(elem) 
+                ret.append((slowo, elem)) 
     return ret
 
 
@@ -45,9 +46,9 @@ if __name__ == '__main__':
     #znajdz_czas()
     print(znajdz_czas())
     #czas = 'zabił'
-    #with open("../bazy/zabil.all", "w") as o:
-    #    for slowo in  odmiany_synonimow(synonimy(czas)):
-    #        print(slowo, file=o)
+    #with open("./bazy/zabil.all", "w") as o:
+    #  for slowo in  odmiany_synonimow(synonimy(czas)):
+    #       print(slowo, file=o)
 #print(znajdz_czas(czas))
 #print(podaj_zadania("Avdads.\nasdas asdkjsa.\n asdjkasdk.\n"))
 #x = podaj_zadania(open("Kennedy.txt").read())
