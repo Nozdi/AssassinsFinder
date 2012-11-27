755cd02547e96c4226f1f57cf58ad758b5ca635e
fred <sylwek1992@gmail.com>
2012-11-27 13:03:08 +0100

    kosmetyka :*

--- finder/finder.py
+++ finder/finder.py
@@ -61,7 +61,7 @@ def potw_presup(name, miejsce, tekst):
             ifname = True
         if miejsce[:-2] in line:
             ifplace = True
-    if ifname and ifplace: print('Nazwisko %s oraz miejsce %s <font color="red"><b>Presupozycja potwierdzona.</b></font>' % (name, miejsce), file=open('temp','a'));return 3 #presupozycja potwierdzona
+    if ifname and ifplace: print('<font color="red"><b>Presupozycja potwierdzona:</b></font><br>Nazwisko %s oraz miejsce %s się zgadzają.' % (name, miejsce), file=open('temp','a'));return 3 #presupozycja potwierdzona
     elif ifname: print('Nazwisko %s <b>potwierdzone</b>' % name, file=open('temp','a'));return 2 
     elif ifplace: print('Miejsce %s <b>potwierdzone</b>' % miejsce, file=open('temp','a'));return 1
     else: print('Presupozycja <b>nie potwierdzona</b>', file=open('temp','a'));return 0#tekst nie zawiera dostatecznych informacji
